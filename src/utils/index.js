23859dede53452f073fd0f6baaa17aa26552917d
Douglas Gabriel Ochner <douglas.ochner.ext@spms.min-saude.pt>
2023-01-16 20:08:34 +0000

    feat: add function to generate uuidv4

--- src/utils/index.js
+++ src/utils/index.js
@@ -115,3 +115,17 @@ export function toDecimal(num, decimals) {
   // Remove trailing zeroes from fractional part before returning
   return finalNumber.replace(/0+$/, '').replace('.+$', ',')
 }
+
+/**
+ * Generates a UUID v4
+ * @return {string}
+ * @see https://stackoverflow.com/a/2117523/1709587
+ * @example
+ * generateUuid() // '7982fcfe-5721-4632-9453-8cf403427d3a'
+ *
+ */
+export function generateUUID() {
+  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16),
+  )
+}
